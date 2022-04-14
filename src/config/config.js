98529b60d236f60d65526934f5744b9cef25b4b3
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-15 01:04:36 +0800

    feat: Connect database and test query by api
    Connect to PostgreSQL database, then can send a request to /api/sql, it
    will return current timestamp.

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,37 @@
+import Joi from 'joi';
+
+// 匯入、設定 dotenv，它會去讀取 .env 檔案內容放入 PROCESS.ENV
+require('dotenv').config();
+
+// 建立 joi 驗證規則
+const schema = Joi.object().keys({
+  NODE_ENV: Joi.string().default('development').allow('development', 'production'),
+  PORT: Joi.number().default(8080),
+  VERSION: Joi.string(),
+  POSTGRES_USER: Joi.string(),
+  POSTGRES_HOST: Joi.string().default('localhost'),
+  POSTGRES_DATABASE: Joi.string(),
+  POSTGRES_PASSWORD: Joi.string(),
+  POSTGRES_PORT: Joi.number().default(5432),
+}).unknown().required();
+
+// 提取 .env 檔案內容並做驗證
+const { error, value: env } = schema.validate(process.env);
+
+// 當驗證失敗時拋出錯誤訊息
+if (error) {
+  throw new Error(`Config validation error: ${error.message}`);
+}
+
 const config = {
-  version: '1.0.0',
-  env: 'development',
-  port: '3000',
+  version: env.VERSION, // 版本
+  env: env.NODE_ENV, // 環境
+  port: env.PORT, // 阜號
+  postgresUser: env.POSTGRES_USER, // 資料庫用戶名稱
+  postgresHost: env.POSTGRES_HOST, // 資料庫主機名稱
+  postgresDatabase: env.POSTGRES_DATABASE, // 資料庫名稱
+  postgresPassword: env.POSTGRES_PASSWORD, // 資料庫密碼
+  postgresPort: env.POSTGRES_PORT, // 資料庫阜號
 };
 
 export default config;
--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -1,10 +1,37 @@
+// 主路由
 import express from 'express';
+import { Pool } from 'pg';
 import config from '../../config/config';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send(`此路徑是: localhost:${config.port}/api`);
+});
+
+router.get('/sql', (req, res) => {
+  const pool = new Pool({
+    host: config.postgresHost,
+    user: config.postgresUser,
+    password: config.postgresPassword,
+    database: config.postgresDatabase,
+    port: config.postgresPort,
+    max: 20,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 2000,
+  });
+  pool.connect((error, client, release) => {
+    if (error) {
+      res.send(`連線失敗！ (${error.message})`);
+    }
+    client.query('SELECT NOW()', (err, result) => {
+      release();
+      if (err) {
+        res.send('查詢失敗！');
+      }
+      res.send(result.rows);
+    });
+  });
 });
 
 export default router;

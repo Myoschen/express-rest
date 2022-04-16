0ae5b4c7ac28242a5fb260a8dc14725311062d25
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-17 01:12:47 +0800

    refactor: /sql api and add user crud api
    change /sql to /sql-current-time

--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -1,28 +1,20 @@
 // 主路由
 import express from 'express';
-import { Pool } from 'pg';
 import config from '../../config/config';
 import article from './article.route';
+import user from './user.route';
+import pool from '../modules/index.module';
 
 const router = express.Router();
 
-router.use('/article', article);
+router.use('/articles', article);
+router.use('/users', user);
 
 router.get('/', (req, res) => {
   res.send(`此路徑是: localhost:${config.port}/api`);
 });
 
-router.get('/sql', (req, res) => {
-  const pool = new Pool({
-    host: config.postgresHost,
-    user: config.postgresUser,
-    password: config.postgresPassword,
-    database: config.postgresDatabase,
-    port: config.postgresPort,
-    max: 20,
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000,
-  });
+router.get('/sql-current-time', (req, res) => {
   pool.connect((error, client, release) => {
     if (error) {
       res.send(`連線失敗！ (${error.message})`);

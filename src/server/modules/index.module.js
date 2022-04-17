57fcda71fd04d6d691cb5fbbe11028e4f02bca8f
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-17 18:15:15 +0800

    change database connection config

--- src/server/modules/index.module.js
+++ src/server/modules/index.module.js
@@ -13,6 +13,9 @@ import { Pool } from 'pg';
 // });
 const pool = new Pool({
   connectionString: 'postgres://sidrifgioiucsp:c38c74c218e100ac30e894250785c17e1322fb0a7203025b8ba46c35e2af5fea@ec2-52-54-212-232.compute-1.amazonaws.com:5432/dcccsah1l84ci',
+  ssl: {
+    rejectUnauthorized: false,
+  },
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,

cb32e5e2e7f9aa8035b8bb8f8f97f1c6fe54eefb
Myos <68583272+willy14620@users.noreply.github.com>
2022-04-17 17:57:58 +0800

    change database connection config

--- src/server/modules/index.module.js
+++ src/server/modules/index.module.js
@@ -1,12 +1,18 @@
 import { Pool } from 'pg';
-import config from '../../config/config';
+// import config from '../../config/config';
 
+// const pool = new Pool({
+//   host: config.postgresHost,
+//   user: config.postgresUser,
+//   password: config.postgresPassword,
+//   database: config.postgresDatabase,
+//   port: config.postgresPort,
+//   max: 20,
+//   idleTimeoutMillis: 30000,
+//   connectionTimeoutMillis: 2000,
+// });
 const pool = new Pool({
-  host: config.postgresHost,
-  user: config.postgresUser,
-  password: config.postgresPassword,
-  database: config.postgresDatabase,
-  port: config.postgresPort,
+  connectionString: 'postgres://sidrifgioiucsp:c38c74c218e100ac30e894250785c17e1322fb0a7203025b8ba46c35e2af5fea@ec2-52-54-212-232.compute-1.amazonaws.com:5432/dcccsah1l84ci',
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
